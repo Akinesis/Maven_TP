7486949d80b049241b0da0f2de70051b761d834e
joachim <joachim.vanoni@gmail.com>
2018-06-06 14:40:03 +0200

    Commit du TP 3
    Modification du POM et de la class pour tenir compte des modifications du TP3

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,7 @@
 package dev;
 
+import java.util.ResourceBundle;
+
 import com.github.lalyos.jfiglet.FigletFont;
 
 
@@ -7,8 +9,11 @@ public class App
 {
     public static void main( String[] args )
     {
-    	
-    	String asciiArt = "Hello World !";
-        System.out.println(FigletFont.convertOneLine(asciiArt));
+    	String titre = ResourceBundle.getBundle("application").getString("titre");
+    	//String asciiArt = "Hello World !";
+        System.out.println(FigletFont.convertOneLine(titre));
+        
+        String environnement = ResourceBundle.getBundle("application").getString("environnement");
+        System.out.println("Environement : "+environnement);
     }
 }
